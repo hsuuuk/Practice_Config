b38be6856923bae28f2c8eb79a9d00255c3afd1a
hsuuuk <hsuuuk.dev@gmail.com>
2023-07-11 09:47:42 +0900

    Initial Commit

---
+++ Practice_Config/Practice_ConfigApp.swift
@@ -0,0 +1,17 @@
+//
+//  Practice_ConfigApp.swift
+//  Practice_Config
+//
+//  Created by 심현석 on 2023/07/11.
+//
+
+import SwiftUI
+
+@main
+struct Practice_ConfigApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
